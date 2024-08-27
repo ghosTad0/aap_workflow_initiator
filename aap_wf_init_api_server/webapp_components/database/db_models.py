3ea1adaaca00723a9dca6c91689128eb66a92dab
tad <tuan.a@hsenidmobile.com>
2024-08-27 16:50:15 +0530

    gunicorn logs, db models changes

--- aap_wf_init_api_server/webapp_components/database/db_models.py
+++ aap_wf_init_api_server/webapp_components/database/db_models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, UUID
+import uuid
 from sqlalchemy.orm import Mapped, mapped_column
 from aap_wf_init_api_server.webapp_components.database.db_config import db
 
@@ -6,3 +7,19 @@ class User(db.Model): # type: ignore
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(20), unique=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
+
+class repo_syncs(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    repo_url: Mapped[str] = mapped_column(String(200))
+
+class associated_workflows(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    workflow_url = Mapped[str] = mapped_column(String(200))
+
+class workflow_triggers(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(String(20))
+    filenames: Mapped[str] = mapped_column(String(200))
+
+# Associated workflow : workflow triggers = one : many
+# repo_sync: associated_workflow = one : many

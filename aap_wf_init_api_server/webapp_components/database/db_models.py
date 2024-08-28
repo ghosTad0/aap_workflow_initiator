82a52d929b5b448906acfa0e57efad075a39e809
tad <tuan.a@hsenidmobile.com>
2024-08-28 15:54:25 +0530

    building entire db schema

--- aap_wf_init_api_server/webapp_components/database/db_models.py
+++ aap_wf_init_api_server/webapp_components/database/db_models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Integer, String, UUID
+from sqlalchemy import Integer, String, UUID, ForeignKey, Table, Column
+from sqlalchemy.orm import relationship
 import uuid
+from typing import List
 from sqlalchemy.orm import Mapped, mapped_column
 from aap_wf_init_api_server.webapp_components.database.db_config import db
 
@@ -8,18 +10,38 @@ class User(db.Model): # type: ignore
     username: Mapped[str] = mapped_column(String(20), unique=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
 
+
 class repo_sync(db.Model):
+    __tablename__ = "repo_sync_table"
     id: Mapped[int] = mapped_column(primary_key=True)
     repo_url: Mapped[str] = mapped_column(String(200))
+    workflows: Mapped[List["workflow"]] = relationship(back_populates="repo_sync")
+
+
+association_table_wf_and_wftriggers = Table(
+    "association_table_wf_and_wftriggers",
+    db.Model.metadata,
+    Column("workflow_id", ForeignKey("workflow_table.id"), primary_key=True),
+    Column("workflow_trigger_id", ForeignKey("workflow_trigger_table.id"), primary_key=True),
+)
+
 
 class workflow(db.Model):   #associated workflow
+    __tablename__ = "workflow_table"
     id: Mapped[int] = mapped_column(primary_key=True)
-    workflow_url = Mapped[str] = mapped_column(String(200))
+    workflow_url: Mapped[str] = mapped_column(String(200))
+    repo_sync_id: Mapped[int] = mapped_column(ForeignKey("repo_sync_table.id"))
+    workflow_triggers: Mapped[List["workflow_trigger"]] = relationship(secondary=association_table_wf_and_wftriggers, back_populates="workflow")
+
 
 class workflow_trigger(db.Model):
+    __tablename__ = "workflow_trigger_table"
     id: Mapped[int] = mapped_column(primary_key=True)
+    trigger_name: Mapped[str] = mapped_column(String(100))
     type: Mapped[str] = mapped_column(String(20))
     filenames: Mapped[str] = mapped_column(String(200))
+    workflows: Mapped[List["workflow"]] = relationship(secondary=association_table_wf_and_wftriggers, back_populates="workflow_trigger")
+
 
 # Associated workflow : workflow triggers = one : many
 # repo_sync: associated_workflow = one : many
